5ea39d3f6cc624568f8cef7661bf41c36d135a2e
sumon <18103159@iubat.edu>
2023-12-10 19:29:55 +0600

    url mm

--- routes/api.php
+++ routes/api.php
@@ -1,9 +1,12 @@
 <?php
 
-use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Api\AuthController;
 
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+# Authentication(Login)
+Route::post('/login',[AuthController::class,'login']);
+Route::middleware('auth:sanctum')->group(function(){
+    # Authentication(LogOut)
+    Route::get('/logout',[AuthController::class,'logout']);
 });
