253d2cdaff87501a86c0a3ef634e9293d4a13e13
sumon <18103159@iubat.edu>
2023-12-11 17:09:06 +0600

    registration

--- routes/web.php
+++ routes/web.php
@@ -10,15 +10,13 @@ Route::get('/',[UrlController::class,'create'])->name('url.create');
 #authLogin
 Route::get('/login',[AuthController::class,'login'])->name('user.login');
 Route::post('/login_post',[AuthController::class,'loginPost'])->name('user.login.post');
+#Registration
+Route::get('/registration',[AuthController::class,'registration'])->name('user.registration');
+Route::post('/registration',[AuthController::class,'registrationPost'])->name('user.registration.post');
 
 Route::middleware(['auth'])->group(function () {
     #logout
     Route::get('/logout',[AuthController::class,'logout'])->name('user.logout');
-
-    #Registration
-    Route::get('/registration',[AuthController::class,'registration'])->name('user.registration');
-    Route::post('/registration',[AuthController::class,'registrationPost'])->name('user.registration.post');
-
 
     #user
     Route::get('/profile',[UserController::class,'profile'])->name('user.profile');
